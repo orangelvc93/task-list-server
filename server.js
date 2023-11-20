434e46ab8750605e49df503b25e70fcfa9ca8698
Orangel Valdespino <orangelvc93@gmail.com>
2023-11-20 14:39:06 -0500

    activad proyect-2 v1

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
 const express = require('express');
 const app = express();
+const rutasTareas = require('./list-view-router');
+
 
 const PORT = 3000;
 
-
-app.get('/', (req, res) => {
-    res.json(
-        [
-            {
-                "id": 123456,
-                "isCompleted": false,
-                "description": "Pasear al perro",
-            },
-            {
-                "id": 123457,
-                "isCompleted": false,
-                "description": "Preparar almuerzo",
-            },
-            {
-                "id": 123458,
-                "isCompleted": false,
-                "description": "Ir al gimnasio",
-            },
-            {
-                "id": 123459,
-                "isCompleted": false,
-                "description": "Practicar programación",
-            }
-        ]
-    )
-})
-
-
+app.use("/", rutasTareas)
 
 app.listen(PORT, () => {
     console.log(`listening on port http://localhost:${PORT}`);
