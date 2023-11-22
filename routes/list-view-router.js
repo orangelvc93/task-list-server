2dbd379431668f0d4bf8b851cb8ff3c039f3de7d
Orangel Valdespino <orangelvc93@gmail.com>
2023-11-22 09:50:24 -0500

    Modificando archivo con middleware

--- routes/list-view-router.js
+++ routes/list-view-router.js
@@ -1,27 +1,50 @@
-const { Router } = require("express");
-const { listado } = require("../db/lista-tareas");
+const { Router } = require('express');
 const router = Router();
+
+
+/* const listaTareas = [
+    {
+        "id": 123457,
+        "isCompleted": true,
+        "description": "Preparar almuerzo",
+    },
+    {
+        "id": 123458,
+        "isCompleted": false,
+        "description": "Ir al gimnasio",
+    },
+    {
+        "id": 123459,
+        "isCompleted": false,
+        "description": "Practicar programación",
+    }
+] */
 
 
 //Hacer una solicitud GET a una ruta específica para listar las todas tareas.
 router.get('/list', (req, res) => {
-    res.json(listado())
+    const listaTareas = req.app.locals.listaTareas;
+    res.json(listaTareas)
 })
 
-//Hacer una solicitud GET a una ruta específica para ver una tarea en especifico
-router.get('/list_id/:id', (req, res) => {
-    const listId = req.params.id;
-    const filtro_id = listado.filter(tarea => tarea.id === parseInt(listId))
-    res.json(filtro_id[0]);
+//Hacer una solicitud GET a una ruta específica para ver una tarea en especifico.
+router.get('/list/:id', (req, res) => {
+    const listaTareas = req.app.locals.listaTareas;
+    const tareaId = parseInt(req.params.id);
+    const tareaEspecifica = listaTareas.find(tarea => tarea.id === tareaId);
+
+    if (tareaEspecifica) {
+        res.json(tareaEspecifica)
+    } else {
+        res.status(404).json({ msj: `La tarea con id ${tareaId} no existe.` })
+    }
 })
 
-//Hacer una solicitud GET a una ruta específica para filtrar por tareas completas o incompletas
-router.get('/list_state/:isCompleted', (req, res) => {
-    const isCompleted = req.params.isCompleted === 'true';
-    const listaFiltrada = listado.filter(tarea => tarea.isCompleted === isCompleted);
-    res.json(listaFiltrada);
-});
+
+
 
 
 
 module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const app = express();
-const rutasTareas_view = require('./routes/list-view-router');
-const rutasTareas_edit = require('./routes/list-edit-router');
-
+// Importamos los enrutadores
+const rutasTareas_view = require('./routes/list-view-router')
 
 const PORT = 3000;
 
-app.use(express.json())
 
-app.use("/lista_view", rutasTareas_view)
-app.use("/lista_edit", rutasTareas_edit)
+// Función middleware para almacenar el array de tareas
+function middleware(req, res, next) {
+    // Almacenamos el array de tareas en la memoria
+    req.app.locals.listaTareas = [
+        {
+            "id": 123457,
+            "isCompleted": true,
+            "description": "Preparar almuerzo",
+        },
+        {
+            "id": 123458,
+            "isCompleted": false,
+            "description": "Ir al gimnasio",
+        },
+        {
+            "id": 123459,
+            "isCompleted": false,
+            "description": "Practicar programación",
+        }
+    ];
+
+    next();
+}
+//Registramos el Middleware
+app.use(middleware);
+
+// Usamos el array de tareas almacenado en el middleware
+app.use('/view', rutasTareas_view)
+
+
+
 
 app.listen(PORT, () => {
-    console.log(`listening on port http://localhost:${PORT}`);
-});
+    console.log(`Servidor respondiendo en el puerto http://localhost:${PORT}`);
+})
+
+
+
